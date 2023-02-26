73258537899d0fd6ec05af0f899216c98a9ccc14
Wansaaa <ichwansanusi60@gmail.com>
2023-02-26 23:10:00 +0700

    Mengubah icon button

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 var a;
 function show_hide() {
   if (a == 1) {
-    document.getElementById('myImage').src = 'img/kue.jpg';
+    document.querySelector('#myImage').src = 'img/kue.jpg';
     return (a = 0);
   } else {
-    document.getElementById('myImage').src = 'img/kue.png';
+    document.querySelector('#myImage').src = 'img/kue.png';
     return (a = 1);
   }
 }
